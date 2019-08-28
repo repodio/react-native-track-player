c2cf9015bcd3c360375165b9ad51e91bca436956
Thorben Primke <thorben@primke.com>
2019-08-28 09:17:37 -0700

    Support For Providing A StartPosition With Track
    Summary:
    This adds support for providing a startPosition with a track. If a startPosition
    is provided, the track will start playing from the specified position.
    The position is in seconds.
    
    I chose to name it startPosition because we may want to add support for providing
    an endPosition as well in order to only play a sub-clip of a track. It’s not part of this
    PR as it requires additional changes to SwiftAudio on the iOS side. ExoPlayer on the
    Android side already supports this.
    
    Test Plan:
    - verified on both iOS and Android with the example app
    - used a new sound file that counts in order to easily verify that startPosition works as expected

--- index.d.ts
+++ index.d.ts
@@ -69,6 +69,7 @@ declare namespace RNTrackPlayer {
     contentType?: string;
     pitchAlgorithm?: PitchAlgorithm;
     [key: string]: any;
+    startPositionSec: number;
   }
 
   export interface PlayerOptions {
