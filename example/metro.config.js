ac64ee6fb2d03f6d75599f9ed87f13ca07d0802e
Thorben Primke <thorben@primke.com>
2019-12-21 15:13:04 -0800

    Updates Example To Do Local Dev Against Lib Directly

--- example/metro.config.js
+++ example/metro.config.js
@@ -5,13 +5,33 @@
  * @format
  */
 
+const path = require("path");
+const blacklist = require("metro-config/src/defaults/blacklist");
+
+const reactNativeLib = path.resolve(__dirname, "..");
+
 module.exports = {
+  watchFolders: [path.resolve(__dirname, "node_modules"), reactNativeLib],
+
+  resolver: {
+    // https://github.com/facebook/metro/issues/7#issuecomment-508129053
+    // https://github.com/facebook/react-native/issues/21310
+    extraNodeModules: new Proxy({}, {
+      get: (target, name) => path.join(process.cwd(), `node_modules/${name}`),
+    }),
+    blacklistRE: blacklist([
+      new RegExp(`${reactNativeLib}/node_modules/react-native/.*`)
+    ])
+  },  
+  
+  projectRoot: path.resolve(__dirname),
+
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
+        inlineRequires: false
+      }
+    })
   },
 };
