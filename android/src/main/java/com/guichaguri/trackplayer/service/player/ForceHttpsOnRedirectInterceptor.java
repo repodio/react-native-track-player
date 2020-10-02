dfb819ac2a3899d0483272ce76a14cdcf1d40b20
Thorben Primke <thorben@primke.com>
2020-10-02 13:51:35 -0700

    Fix For ForceHttpsOnRedirectInterceptor

--- android/src/main/java/com/guichaguri/trackplayer/service/player/ForceHttpsOnRedirectInterceptor.java
+++ android/src/main/java/com/guichaguri/trackplayer/service/player/ForceHttpsOnRedirectInterceptor.java
@@ -1,41 +1,26 @@
 package com.guichaguri.trackplayer.service.player;
 
-import android.util.Log;
-
 import java.io.IOException;
 
 import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
-import okhttp3.Request;
 import okhttp3.Response;
 
 public class ForceHttpsOnRedirectInterceptor implements Interceptor {
 
     @Override public Response intercept(Interceptor.Chain chain) throws IOException {
-        Request request = chain.request();
-
-        long t1 = System.nanoTime();
-        Log.d("Test", String.format("Sending request %s", request.url()));
-
-        Response response = chain.proceed(request);
-
-        long t2 = System.nanoTime();
-        Log.d("Test", String.format("Received response for %s", response.request().url()));
-
-        if (!response.request().url().isHttps()) {
-            Request originalRequest = chain.request();
-            HttpUrl newUrl = response.request().url().newBuilder()
-                    .scheme("https")
-                    .build();
-            Request updatedRequest = originalRequest.newBuilder()
-                    .url(newUrl)
-                    .build();
-            response = response.newBuilder()
-                    .request(updatedRequest)
-                    .build();
-            Log.d("Test", String.format("Updated Redirect response for %s", response.request().url()));
-        } else {
-            Log.d("Test", String.format("URL was already https"));
+        Response response = chain.proceed(chain.request());
+        String location = response.headers().get("Location");
+        if (location != null) {
+            HttpUrl httpUrlLocation = HttpUrl.parse(location);
+            if (httpUrlLocation != null && !httpUrlLocation.isHttps()) {
+                response = response.newBuilder()
+                        .addHeader(
+                                "Location",
+                                httpUrlLocation.newBuilder().scheme("https").build().toString()
+                        )
+                        .build();
+            }
         }
         return response;
     }
